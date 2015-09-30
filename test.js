8fc581074f94a2b8923ea76aa0df6c9deff5b70e
pemrouz <p3pedram@hotmail.com>
2015-09-30 22:31:18 +0100

    work for function and simple values

--- test.js
+++ test.js
@@ -2,8 +2,15 @@ var expect = require('chai').expect
   , nullify = require('./')
 
 describe('nullify', function() {
+
   it('should invert result of function', function() {
     var a = [0, 1]
     expect(a.map(nullify(Boolean))).to.eql([null, true])
   })
+
+  it('should nullify value', function() {
+    expect(nullify(0)).to.eql(null)
+    expect(nullify(1)).to.eql(true)
+  })
+
 })
